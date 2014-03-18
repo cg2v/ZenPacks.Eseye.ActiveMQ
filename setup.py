b207ea854d4c7c186b5ad5421529baf4309d469c
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2014-03-18 16:23:52 -0400

    Tag as 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.Eseye.ActiveMQ"
-VERSION = "0.8"
+VERSION = "0.9"
 AUTHOR = "Jose Salvatierra"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.Eseye']
